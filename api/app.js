1eafb20c34cfc87eb84d191d82615c856ded60d6
npasquier <nicolas.pasquier@mythalesgroup.io>
2020-12-03 18:58:38 +0100

    all done, no tests

---
+++ api/app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { allSongs } from "./assets/songs";
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(function(_req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+app.get('/:search', (req, res) => {
+  // the regex is used to send an empty list if the search is empty or contains only whitespaces
+  const songsFound = req.params.search.replace(/\s/g, '').length > 0
+        ?
+      allSongs.filter(song => song.toLowerCase().includes(req.params.search.toLowerCase()))
+        :
+      []
+  res.status(200).json(songsFound)
+})
+
+app.get('/', (req, res) => {
+  res.status(200).json([])
+})
+
+export default app
---
+++ client/src/components/SongSearch/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react"
+import axios from 'axios'
+import SongList from "../SongList"
+
+const SongSearch = props => {
+    const [search, setSearch] = useState('')
+    const [songList, setSongList] = useState([])
+
+    const fetchSongs = (search) => {
+        axios.get('http://localhost:8081/'+search)
+            .then(res => {setSongList(res.data)})
+    }
+
+    return <div>
+        <input
+            type={"text"}
+            size={25}
+            placeholder={"Rechercher un titre ..."}
+            value={search}
+            onChange={e => {
+                fetchSongs(e.target.value)
+                setSearch(e.target.value)
+            }}
+        />
+        <SongList
+            list={songList}
+            handleSongClicked={props.handleSongClicked}
+        />
+    </div>
+}
+
+export default SongSearch
